a14235daa0fb20b57a9c88d07a7c0a406bcebb0f
OskrrGn <155512833+OskrrGn@users.noreply.github.com>
2025-03-10 22:53:57 -0600

    HolaToast.kt

--- TareaHolaToast/HolaToast.kt
+++ TareaHolaToast/HolaToast.kt
@@ -1,21 +1,3 @@
-package com.example.myapplication
-
-import android.os.Bundle
-import android.widget.Toast
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Button
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
